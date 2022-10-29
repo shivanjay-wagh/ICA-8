c5527123e2f2c844b6bd90d01f251ddc33aa810d
Shivanjay <swagh5@asu.edu>
2022-10-28 18:32:41 -0700

    ====== Shivanjay Wagh == GoodString Method ---> Empty string test =======

--- test/urinalsTest.java
+++ test/urinalsTest.java
@@ -24,6 +24,13 @@ class urinalsTest{
         assertTrue(check1);
     }
 
+    @Test
+    @DisplayName("====== Omkar Pisal == Good String Method -> bad string test =======")
+    void is_Bad_String() {
+        boolean check2 = urinals.goodString("0101100");
+        assertFalse(check2);
+    }
+
 
 
 
