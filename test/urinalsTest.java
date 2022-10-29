229dbd8e3772906c79f27c900626a683af5455b1
Shivanjay <swagh5@asu.edu>
2022-10-28 18:32:20 -0700

    ====== Shivanjay Wagh == GoodString Method ---> Empty string test =======

--- test/urinalsTest.java
+++ test/urinalsTest.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class urinalsTest{
@@ -15,4 +16,18 @@ class urinalsTest{
         boolean check = urinals.goodString(" ");
         assertFalse(check);
     }
+
+    @Test
+    @DisplayName("====== Omkar Pisal == Good String Method -> good string test =======")
+    void is_Good_String() {
+        boolean check1 = urinals.goodString("010101");
+        assertTrue(check1);
+    }
+
+
+
+
+
+
+
 }
